a3bf083f7a6b97d1a5aa0cffca5f9f349f8fdb2b
leonardo-fabricio <leonardo.fabricio@escolar.ifrn.edu.br>
2021-11-17 21:21:18 -0300

    alterando title

--- integrador_6_periodo/urls.py
+++ integrador_6_periodo/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
-schema_view = get_swagger_view(title='Pastebin API')
+schema_view = get_swagger_view(title='RN Empregos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
