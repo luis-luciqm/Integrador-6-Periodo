fc1eb4d7870ddb7e84d96e63f8d8076a48db9102
leonardo-fabricio <leonardo.fabricio@escolar.ifrn.edu.br>
2021-12-02 12:20:58 -0300

    change template email and message error

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.views import LoginView,PasswordResetView
+from django.shortcuts import redirect
 from django.views.generic.edit import CreateView
 from django.contrib import messages
 from .forms import *
@@ -35,7 +36,28 @@ class PasswordReset(SuccessMessageMixin, PasswordResetView):
     email_template_name = 'accounts/password_reset_email.html'
     html_email_template_name = 'accounts/password_reset_email.html'
     subject_template_name = 'accounts/password_reset_subject.txt'
-    success_url = '/accounts/password_reset/done/'
+    success_url = '/accounts/password_reset/done'
+    
+    def form_valid(self, form):
+        #objects
+        email = User.objects.filter(email = form.cleaned_data['email']).exists()
+        if not email:
+            messages.error(self.request, 'Email não consta em nossa base')
+            return redirect('/accounts/password_reset/')
+        
+        opts = {
+            'use_https': self.request.is_secure(),
+            'token_generator': self.token_generator,
+            'from_email': self.from_email,
+            'email_template_name': self.email_template_name,
+            'subject_template_name': self.subject_template_name,
+            'request': self.request,
+            'html_email_template_name': self.html_email_template_name,
+            'extra_email_context': self.extra_email_context,
+        }
+        form.save(**opts)
+        return super().form_valid(form)
+        
     
 class PasswordResetDone(SuccessMessageMixin, PasswordResetDoneView):
     template_name = 'accounts/password_reset_done.html'
