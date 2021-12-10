7d59e12b1d190b00b3553c59ce1f4b2fda7e1ba6
leonardo-fabricio <leonardo.fabricio@escolar.ifrn.edu.br>
2021-12-10 13:36:21 -0300

    configurando permissoes de grupo empresa

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,12 +1,19 @@
+from django.db.models.query_utils import Q
 from django.shortcuts import redirect
 from accounts.models import Solicitation
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, Permission
 
 def ApproveSolicitation(request, pk):
     solicitation = Solicitation.objects.get(pk=pk)
     solicitation.accept = True
-    my_group, created = Group.objects.get_or_create(name='Empresa') 
-    solicitation.user.groups.add(my_group)
     
+    my_group, created = Group.objects.get_or_create(name='Empresa') 
+    permission = Permission.objects.get(codename = 'add_announcement')
+    my_group.permissions.add(permission)
+    permission = Permission.objects.get(codename = 'change_announcement')
+    my_group.permissions.add(permission)
+    
+    solicitation.user.groups.add(my_group)
     solicitation.save()
+    
     return redirect('/accounts/listar_solicitacoes/')
